cd5feba617015eb7bd927721960c8bd6e5ec692c
Jay Bansal <jay99bansal@gmail.com>
2018-06-25 14:01:23 +0530

    a

---
+++ def.cpp
@@ -0,0 +1,9 @@
+//
+//  def.cpp
+//  test_mf
+//
+//  Created by Jay Bansal on 25/06/18.
+//  Copyright © 2018 Jay Bansal. All rights reserved.
+//
+
+#include "def.hpp"
---
+++ def.hpp
@@ -0,0 +1,14 @@
+//
+//  def.hpp
+//  test_mf
+//
+//  Created by Jay Bansal on 25/06/18.
+//  Copyright © 2018 Jay Bansal. All rights reserved.
+//
+
+#ifndef def_hpp
+#define def_hpp
+
+#include <stdio.h>
+
+#endif /* def_hpp */
