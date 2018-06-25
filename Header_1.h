f94c667d5e517aad83cdd24561d780ef5f8b024a
Jay Bansal <jay99bansal@gmail.com>
2018-06-25 10:36:09 +0530

    added Header_1.h

---
+++ Header_1.h
@@ -0,0 +1,17 @@
+//
+//  Header_1.h
+//  test_mf
+//
+//  Created by Jay Bansal on 25/06/18.
+//  Copyright © 2018 Jay Bansal. All rights reserved.
+//
+
+#ifndef Header_1_h
+#define Header_1_h
+
+int square(int x)
+{
+    return x*x;
+}
+
+#endif /* Header_1_h */
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_mf
+//
+//  Created by Jay Bansal on 25/06/18.
+//  Copyright © 2018 Jay Bansal. All rights reserved.
+//
+
+#include <iostream>
+#include "Header_1.h"
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << square(3);
+    return 0;
+}
